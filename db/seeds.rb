30266e6778bdc694dff0c645a69b49b5f18df7b9
Coralie Loheac <coralie.loheac@gmail.com>
2019-11-14 15:21:04 +0100

    before scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
+# require 'json'
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "orange")
+Ingredient.create(name: "basil")
+Ingredient.create(name: "egg yolk")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "hay")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "tomato")
+Ingredient.create(name: "amaretto")
+Ingredient.create(name: "champagne")
+Ingredient.create(name: "coffee")
+Ingredient.create(name: "coconut juice")
+Ingredient.create(name: "cane sugar")
